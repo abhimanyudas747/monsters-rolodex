23af34f089c6592bae277e60cfb0e54368f1be1b
Abhimanyu Das <abhimanyu.das34@gmail.com>
2020-06-06 21:22:27 +0530

    Updates

---
+++ precache-manifest.a51832c3fe83cbf2cf3860bc38b139ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412f74d4979339c2bf3bd01aa32b748e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "931ffe0cec8368ebe1f4",
+    "url": "/monsters-rolodex/static/css/main.35c431f7.chunk.css"
+  },
+  {
+    "revision": "b5753794ed0a583090f2",
+    "url": "/monsters-rolodex/static/js/2.782ae6f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.782ae6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "931ffe0cec8368ebe1f4",
+    "url": "/monsters-rolodex/static/js/main.4a3e629f.chunk.js"
+  },
+  {
+    "revision": "f4317a139f8b98a0e347",
+    "url": "/monsters-rolodex/static/js/runtime-main.da7a1209.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
